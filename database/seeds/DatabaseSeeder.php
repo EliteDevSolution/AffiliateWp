af75330ff69cf3e55b30ac85d0ed685d819e9cb0
EliteDevSolution <fstorm707@gmail.com>
2024-08-16 21:41:17 +0900

    Update: AdsImageSeed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeed::class);
         $this->call(UserSeed::class);
         $this->call(SocialTableSeed::class);
-        $this->call(AdsImage::class);
+        $this->call(AdsImageSeed::class);
     }
 }
