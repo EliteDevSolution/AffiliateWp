d9c00d64931611458cd04ec4ba65453a06674534
EliteDevSolution <fstorm707@gmail.com>
2024-08-20 18:50:30 +0900

    fixed: auth  middleware

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            if (Auth::user()->hasRole("administrator"))
+            if (Auth::user())
                 return redirect()->to("/");
         }
         $response = $next($request);
